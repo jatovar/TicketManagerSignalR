6ed586cef47faef91a057626ca1ef347a7b07ae4
Administrador <administrador@MacBook-Pro-de-Jorge.local>
2018-04-12 18:28:07 -0500

    add fix 1 cors

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace SEPSignalR
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options => options.AddPolicy("CorsPolicy", builder => { builder .AllowAnyMethod() .AllowAnyHeader() .WithOrigins("http://localhost:5000"); })); 
+            //services.AddCors(options => options.AddPolicy("CorsPolicy", builder => { builder .AllowAnyMethod() .AllowAnyHeader() .WithOrigins("http://localhost:5000"); })); 
             services.AddSignalR();
             services.AddScoped<MesaDeTicketsHub>();
             services.AddSingleton<TicketManager>();
@@ -25,7 +25,7 @@ namespace SEPSignalR
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors("CorsPolicy");
+            app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseFileServer();
             app.UseSignalR(routes =>
             {
